56fb696cb31940ec7d94a6a065e48bef65a990a8
Rob Rimmer <developer@atrico.net>
2015-09-23 10:02:40 +0100

    Convert to WPF project Started EnumComboBox

---
+++ Atrico.Lib.WPF.Tests/TestEnumComboBox.cs
@@ -0,0 +1,27 @@
+﻿using Atrico.Lib.Assertions;
+using Atrico.Lib.Assertions.Constraints;
+using Atrico.Lib.Assertions.Elements;
+using Atrico.Lib.Testing;
+using Atrico.Lib.Testing.TestAttributes.NUnit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atrico.Lib.Dimensions.Tests
+{
+    [TestFixture]
+    public class TestEnumComboBox : TestFixtureBase
+    {
+        private static object CreateControlWindow()
+        {
+            
+        }
+        [Test]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
